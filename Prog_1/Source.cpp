19dcb7cf30b8dfcede1627943804d96c6e52cbeb
d104-20 <d104-20@D104-20>
2019-02-11 14:18:02 +0100

    asd

---
+++ Prog_1/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int szul, akt = 2019;
+	cout << "Adja meg az szuetesi evet!" << endl;
+	cin >> szul;
+	cout << "Az akt eletkora kb. " << akt - szul << " ev! \n";
+}

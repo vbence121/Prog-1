b2c41c48df01d9f683e7439cfcfa2837ff93df74
d104-20 <d104-20@D104-20>
2019-02-11 14:28:44 +0100

    asd

--- Prog_1/Source.cpp
+++ Prog_1/Source.cpp
@@ -8,4 +8,12 @@ int main() {
 	cout << "Adja meg az szuetesi evet!" << endl;
 	cin >> szul;
 	cout << "Az akt eletkora kb. " << akt - szul << " ev! \n";
+
+	int ev = szul;
+	while (ev != akt) {
+		cout << ev << endl;
+		ev = ev + 1;
+	}
+
+	cout << "Viszlát!\n";
 }

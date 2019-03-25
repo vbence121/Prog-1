220e2ca8cfccfb39fcd8731b34b5a903d7757592
d104-20 <d104-20@D104-20>
2019-03-25 14:34:41 +0100

    ass

--- Prog_1/ora8.cpp
+++ Prog_1/ora8.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct GPS {
@@ -11,6 +12,21 @@ struct Varos {
 	GPS koord;
 };
 
+void kiirVaros(Varos v) {
+	cout << v.nev << "\t(" << v.koord.szelfok << "," << v.koord.hosszfok << ")\t";
+	cout << v.lakossag << " lakos \n";
+}
+void kiirVarosok(Varos* varosok, int db) {
+	for (int i = 0; i < db; i++) {
+		kiirVaros(varosok[i]);
+	}
+}
+
+// lakossag szam szerinti sorrendben rendez
+void rendez(Varos tomb[], int db) {
+	
+}
+
 int main() {
 
 	int szam;
@@ -22,6 +38,13 @@ int main() {
 	szulovaros.koord.szelfok= 17.25;
 	int szamok[] = { 1, 2, 3 };
 	Varos fovaros = { "Budapest", 1749734, 47.49, 19.04 };
-
+	Varos varosok[] =
+	{
+		szulovaros,
+		fovaros,
+		{"Gyor", 130094, {47.69, 17.64}},
+		{"Bécs", 1840573, {48.21, 16.37}},
+		{"Pozsony", 425923, {48.15, 17.12}}
+	};
 
 }

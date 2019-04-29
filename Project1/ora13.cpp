d1d99d3405e37215e93e359e2b8738c08c55315b
d104-02 <d104-02@D104-02>
2019-04-29 14:33:58 +0200

    lkúűő

--- Project1/ora13.cpp
+++ Project1/ora13.cpp
@@ -8,7 +8,57 @@ struct Elem {
 	Elem* next;
 };
 
+struct Lista {
+	Elem* head;
+	int count;
+	Elem* tail;
+};
+
+Elem* addFirst(Elem* head, string data) {
+	Elem* uj = new Elem;
+	uj->data = data;
+	uj->next = head;
+	return uj;
+}
+
+void printlist(Elem* head) {
+	cout << "A lista tartalma: \n";
+	while (head) {
+		cout << head->data << endl;
+		head = head->next;
+	}
+}
+
+Elem* deleteFirst(Elem* head) {
+	if (!head) return head;
+	cout << "Task " << head->data << "is done.\n" << endl;
+	Elem* kov = head->next;
+	delete head;
+	return kov;
+}
+
+void deleteList(Elem* head) {
+	while (head) {
+		Elem* kov = head->next;
+		delete head;
+		head = kov;
+		// vagy deleteFirst(head);
+	}
+}
 
 int main() {
-
+	Elem* head = nullptr;
+	string input;
+	do {
+		getline(cin, input);
+		if (input == "exit")
+			break;
+		if (input == "done" ) {
+			head = deleteFirst(head);
+			continue;
+		}
+		head = addFirst(head, input);
+		printlist(head);
+	} while (input!="exit");
+	deleteList(head);
 }

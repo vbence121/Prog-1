a3088cc86cbda6d703987fbc96f0c216f92df88b
d104-02 <d104-02@D104-02>
2019-04-29 15:34:31 +0200

    asdasd

--- Project1/ora13.cpp
+++ Project1/ora13.cpp
@@ -31,7 +31,7 @@ void printlist(Elem* head) {
 
 Elem* deleteFirst(Elem* head) {
 	if (!head) return head;
-	cout << "Task " << head->data << "is done.\n" << endl;
+	cout << "Task " << head->data << " is done.\n";
 	Elem* kov = head->next;
 	delete head;
 	return kov;
@@ -46,6 +46,42 @@ void deleteList(Elem* head) {
 	}
 }
 
+Elem* firstToLast(Elem* head) {
+	if (!head || !head->next) return head;
+	Elem* akt = head->next;
+	Elem* ujelso = head->next;
+	while (akt->next) {
+		akt = akt->next;
+	}
+	akt->next = head;
+	head->next = nullptr;
+	return ujelso;
+}
+
+Elem* removeORAdd(Elem* head, string input) {
+	if (!head) return addFirst(head, input);
+	if (head->data == input) {
+		return deleteFirst(head);
+	}
+	Elem* akt = head;
+	bool van = false;
+	while (akt->next) {
+		if (akt->next->data == input) {
+			Elem* torlendo = akt->next;
+			cout << "Task " << torlendo->data << " is done.\n";
+			akt->next = torlendo->next;
+			delete torlendo;
+			break;
+			}
+		akt = akt->next;
+	}
+	Elem* uj = new Elem;
+	akt->next = uj;
+	uj->data = input;
+	uj->next = nullptr;
+	return head;
+}
+
 int main() {
 	Elem* head = nullptr;
 	string input;
@@ -57,7 +93,14 @@ int main() {
 			head = deleteFirst(head);
 			continue;
 		}
-		head = addFirst(head, input);
+		if (input == "not today") {
+			// elem elso elemet a lista vegere teszi
+			head = firstToLast(head);
+			printlist(head);
+			continue;
+		}
+		//head = addFirst(head, input);
+		head = removeORAdd(head, input); // megkeresi a listaban ha bennne van kitorli ha nincs hozza fuzi a vegehez
 		printlist(head);
 	} while (input!="exit");
 	deleteList(head);
